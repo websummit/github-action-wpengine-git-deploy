64b5f9a59bda3a2466eba58371f905f9e8c2b035
Alex Anikeev <72082638+alex-anikeev@users.noreply.github.com>
2023-04-24 18:04:49 +0200

    Remove debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,11 +31,6 @@ chmod 600 "$WPENGINE_SSH_KEY_PRIVATE_PATH"
 chmod 644 "$WPENGINE_SSH_KEY_PUBLIC_PATH"
 
 git config --global --add safe.directory /github/workspace
-
-ls -al .git/ || true
-cat .git/HEAD || true
-echo $GIT_DIR || true
-git status || true
 git config core.sshCommand "ssh -i $WPENGINE_SSH_KEY_PRIVATE_PATH -o UserKnownHostsFile=$KNOWN_HOSTS_PATH"
 git remote add $WPENGINE_ENV git@$WPENGINE_HOST:$WPENGINE_ENV/$WPENGINE_ENVIRONMENT_NAME.git
 git push -fu $WPENGINE_ENV $BRANCH:$REMOTE_BRANCH
